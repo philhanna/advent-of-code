70b1b96724a5551b6961084a468d176e25b839bd
Phil Hanna <ph1204@gmail.com>
2023-06-18 20:23:36 -0400

    Finished day 1

--- day1/main.go
+++ day1/main.go
@@ -1,120 +1,61 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
+	"log"
+	"os"
 	"sort"
 	"strconv"
-	"strings"
 )
-
-// ---------------------------------------------------------------------
-// Type definitions
-// ---------------------------------------------------------------------
-
-type Elf struct {
-	number int // Elf number (1, 2, ...)
-	total int // Total calories
-}
-
-func (elf *Elf) String() string {
-	return fmt.Sprintf("\tElf %d has total of %d calories", elf.number, elf.total)	
-}
-
-// ---------------------------------------------------------------------
-// Constants and variables
-// ---------------------------------------------------------------------
-
-var elves = make([]Elf, 0)
-
-const data =
-`1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
-`
-
-// ---------------------------------------------------------------------
-// Mainline
-// ---------------------------------------------------------------------
-
 
 func main() {
 
-	elves := loadElves()
-	dumpElves("Original:", elves)
-	sortElves(elves)
-	dumpElves("Sorted:", elves)
-	dumpTop("Top Elf:", elves)
-	dumpTopThree("Sum of top three:", elves)
-}
-func dumpTopThree(label string, elves []Elf) {
-	fmt.Printf("Sum of top 3:\n")
-	fmt.Printf("\t%d calories\n", sumOfTopThree(elves))
-
-}
-// Dump the list of elves
-func dumpElves(label string, elves []Elf) {
-	fmt.Println(label)
-	for _, elf := range elves {
-		fmt.Printf("\tElf %d has total of %d calories\n", elf.number, elf.total)
-	}
-}
-
-// Dumps the top elf
-func dumpTop(label string, elves []Elf) {
-	fmt.Println(label)
-	fmt.Println(elves[0].String())
-}
-
-// Load the calorie data into an array of Elf structures
-func loadElves() []Elf {
-
-	var number int // Current elf number (starts at zero)
-
-	// Create an array of elf calorie totals
-	elves := []Elf{}
-	elves = append(elves, Elf{number, 0})
-
 	// Read the data into the array, converting from strings to integers
-	scanner := bufio.NewScanner(strings.NewReader(data))
-	for scanner.Scan() {
-		text := scanner.Text()
-		if text == "" {
-			number++ // Start the next elf
-			elves = append(elves, Elf{number, 0})
-		} else {
-			calories, _ := strconv.Atoi(text)
-			elves[number].total += calories
+	data, err := os.ReadFile("input.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Make a structure to keep track
+	type Elf struct {
+		number int
+		cals   int
+	}
+	elves := make([]Elf, 0)
+
+	// Split the data on blank lines
+	calorieData := bytes.Split(data, []byte{'\n', '\n'})
+	for i, token := range calorieData {
+
+		// New elf
+		elf := Elf{number: i + 1, cals: 0}
+
+		// Calculate all this elf's calories
+		for _, entry := range bytes.Split(token, []byte{'\n'}) {
+			cals, err := strconv.Atoi(string(entry))
+			if err != nil {
+				log.Fatal(err)
+			}
+			elf.cals += cals
 		}
+
+		// Add this elf to the array
+		elves = append(elves, elf)
 	}
 
-	return elves
-}
+	// Sort the elf array descending by total calories
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].cals > elves[j].cals
+	})
 
-// Sort the elf array in place in descending order of total calories
-func sortElves(elves []Elf) {
-	sortFunction := func(i, j int) bool {
-		return elves[i].total >= elves[j].total
-	}
-	sort.Slice(elves, sortFunction)
-}
+	// Part 1
+	fmt.Printf("Part1: Elf %d has %d calories\n", elves[0].number, elves[0].cals)
 
-// Returns the sum of the calories of the top three elves
-func sumOfTopThree(elves []Elf) int {
-	sum := 0
+	// Part2
+	total := 0
 	for i := 0; i < 3; i++ {
-		sum += elves[i].total
+		total += elves[i].cals
 	}
-	return sum
+	fmt.Printf("Part2: Top 3 elves have %d calories\n", total)
 }
