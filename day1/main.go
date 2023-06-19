4d90b9b663131bb60af871b3185ca86a2025580c
Phil Hanna <ph1204@gmail.com>
2023-06-18 20:36:54 -0400

    Use an array of 2 int instead of a custom type

--- day1/main.go
+++ day1/main.go
@@ -11,12 +11,8 @@ import (
 
 func main() {
 
-	// Make a structure to keep track of elf totals
-	type Elf struct {
-		number int
-		cals   int
-	}
-	elves := make([]Elf, 0)
+	// Make an array to keep track of elf numbers and their total calories
+	elves := make([][2]int, 0)
 
 	// Read the data into the array, converting from strings to integers
 	data, err := os.ReadFile("input.dat")
@@ -29,15 +25,12 @@ func main() {
 	for i, token := range calorieData {
 
 		// New elf
-		elf := Elf{number: i + 1, cals: 0}
+		elf := [2]int{i + 1, 0}
 
 		// Calculate all this elf's calories
 		for _, entry := range bytes.Split(token, []byte{'\n'}) {
-			cals, err := strconv.Atoi(string(entry))
-			if err != nil {
-				log.Fatal(err)
-			}
-			elf.cals += cals
+			cals, _ := strconv.Atoi(string(entry))
+			elf[1] += cals
 		}
 
 		// Add this elf to the array
@@ -46,16 +39,13 @@ func main() {
 
 	// Sort the elf array descending by total calories
 	sort.Slice(elves, func(i, j int) bool {
-		return elves[i].cals > elves[j].cals
+		return elves[i][1] > elves[j][1]
 	})
 
 	// Part 1
-	fmt.Printf("Part1: Elf %d has %d calories\n", elves[0].number, elves[0].cals)
+	fmt.Printf("Part1: Elf %d has %d calories\n", elves[0][0], elves[0][1])
 
 	// Part2
-	total := 0
-	for i := 0; i < 3; i++ {
-		total += elves[i].cals
-	}
+	total := elves[0][1] + elves[1][1] + elves[2][1]
 	fmt.Printf("Part2: Top three elves have %d calories\n", total)
 }
