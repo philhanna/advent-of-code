d137b16b0bbcff2ad96cd8062ec748c965b39021
Phil Hanna <ph1204@gmail.com>
2023-06-18 20:49:32 -0400

    I'm happy with this

--- day1/main.go
+++ day1/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
 	"os"
 	"sort"
 	"strconv"
@@ -11,26 +10,32 @@ import (
 
 func main() {
 
-	// Make an array to keep track of elf numbers and their total calories
-	elves := make([][2]int, 0)
-
-	// Read the data into the array, converting from strings to integers
-	data, err := os.ReadFile("input.dat")
-	if err != nil {
-		log.Fatal(err)
+	// Define a data structure for an Elf.
+	type Elf struct {
+		number int
+		cals   int
 	}
 
-	// Split the data on blank lines
+	// Make an array to keep track of elf numbers and their total
+	// calories.  Each elf entry is an Elf object (elf number and total
+	// calories), and elves is an array of elf entries.
+	elves := make([]Elf, 0)
+
+	// Read the input data into a byte array, with a blank line
+	// separating the entries between the calorie records for each elf.
+	data, _ := os.ReadFile("input.dat")
 	calorieData := bytes.Split(data, []byte{'\n', '\n'})
-	for i, token := range calorieData {
+
+	// Calculate the total calories for each elf
+	for i, calorieEntries := range calorieData {
 
 		// New elf
-		elf := [2]int{i + 1, 0}
+		elf := Elf{number: i + 1, cals: 0}
 
 		// Calculate all this elf's calories
-		for _, entry := range bytes.Split(token, []byte{'\n'}) {
+		for _, entry := range bytes.Split(calorieEntries, []byte{'\n'}) {
 			cals, _ := strconv.Atoi(string(entry))
-			elf[1] += cals
+			elf.cals += cals
 		}
 
 		// Add this elf to the array
@@ -39,13 +44,14 @@ func main() {
 
 	// Sort the elf array descending by total calories
 	sort.Slice(elves, func(i, j int) bool {
-		return elves[i][1] > elves[j][1]
+		return elves[i].cals > elves[j].cals
 	})
 
 	// Part 1
-	fmt.Printf("Part1: Elf %d has %d calories\n", elves[0][0], elves[0][1])
+	topElf := elves[0]
+	fmt.Printf("Part1: Elf %d has %d calories\n", topElf.number, topElf.cals)
 
 	// Part2
-	total := elves[0][1] + elves[1][1] + elves[2][1]
+	total := elves[0].cals + elves[1].cals + elves[2].cals
 	fmt.Printf("Part2: Top three elves have %d calories\n", total)
 }
