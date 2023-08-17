0534a40aafd5093be14c21a7b9b82a57413d3638
Phil Hanna <ph1204@gmail.com>
2023-08-17 17:22:08 -0400

    Added __str__

--- day5/main.py
+++ day5/main.py
@@ -10,6 +10,12 @@ class Stack:
         for stack_id in contents:
             self.contents.append(stack_id)
 
+    def __str__(self) -> str :
+        sb = f"StackNumber {self.number}: {','.join(self.contents)}"            
+        return sb
+        
+
+
 # ============================================================
 # Mainline
 # ============================================================
@@ -20,10 +26,9 @@ if __name__ == '__main__':
     testdataFile = Path(day5).joinpath("testdata.json")
     with open(testdataFile) as fp:
         data = json.load(fp)
-            
+
     # Load the stacks
     stacks = {}
     for stack_number, contents in data['stacks'].items():
-        print(f"{stack_number=} {contents=}")
-
+        stacks[stack_number] = Stack(stack_number, contents)
 
