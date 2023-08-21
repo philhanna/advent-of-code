74122f072b59bde095319680270c254ad2886037
Phil Hanna <ph1204@gmail.com>
2023-08-20 23:28:51 -0400

    Made code a little clearer

--- day5/stacks.go
+++ day5/stacks.go
@@ -11,22 +11,21 @@ type Stacks struct {
 }
 
 func (ps *Stacks) ApplyMove(move Move) error {
+	
+	// Verify that the from and to stack numbers are valid
 	ns := len(ps.CrateStacks)
-
-	// Convert to zero-based stack numbers after checking they are within bounds
-
 	if move.FromStackIndex < 1 || move.FromStackIndex > ns {
 		return fmt.Errorf("From stack number must be between %d and %d, not %d", 1, ns, move.FromStackIndex)
-	}
-	fx := move.FromStackIndex - 1
-
+	}	
 	if move.ToStackIndex < 1 || move.ToStackIndex > ns {
 		return fmt.Errorf("To stack number must be between %d and %d, not %d", 1, ns, move.ToStackIndex)
 	}
+
+	// Convert to zero-based indices
+	fx := move.FromStackIndex - 1
 	tx := move.ToStackIndex - 1
 
 	// Make the move
-
 	for i := 0; i < move.Count; i++ {
 		if ps.CrateStacks[fx].Empty() {
 			return fmt.Errorf("From stack (%d) is empty", move.FromStackIndex)
@@ -36,6 +35,5 @@ func (ps *Stacks) ApplyMove(move Move) error {
 	}
 
 	// Everything ok
-	
 	return nil
 }
