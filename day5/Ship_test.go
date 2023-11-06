1bd3202defef836a873ef896cba17ee5577d5044
Phil Hanna <ph1204@gmail.com>
2023-11-06 16:02:51 -0500

    Got transpose working

--- day5/Ship_test.go
+++ day5/Ship_test.go
@@ -30,12 +30,63 @@ func TestTransposeLines(t *testing.T) {
 		lines []string
 		want  []string
 	}{
-		{"4x3", []string{"abc", "def", "ghi", "jkl",}, []string{"adgj", "behk", "cfil"}},
+		{"4x3", []string{"abc", "def", "ghi", "jkl"}, []string{"adgj", "behk", "cfil"}},
+		{"full",
+			[]string{
+				"[G]                 [D] [R]        ",
+				"[W]         [V]     [C] [T] [M]    ",
+				"[L]         [P] [Z] [Q] [F] [V]    ",
+				"[J]         [S] [D] [J] [M] [T] [V]",
+				"[B]     [M] [H] [L] [Z] [J] [B] [S]",
+				"[R] [C] [T] [C] [T] [R] [D] [R] [D]",
+				"[T] [W] [Z] [T] [P] [B] [B] [H] [P]",
+				"[D] [S] [R] [D] [G] [F] [S] [L] [Q]",
+				" 1   2   3   4   5   6   7   8   9 ",
+			},
+			[]string{
+				"[[[[[[[[ ",
+				"GWLJBRTD1",
+				"]]]]]]]] ",
+				"         ",
+				"     [[[ ",
+				"     CWS2",
+				"     ]]] ",
+				"         ",
+				"    [[[[ ",
+				"    MTZR3",
+				"    ]]]] ",
+				"         ",
+				" [[[[[[[ ",
+				" VPSHCTD4",
+				" ]]]]]]] ",
+				"         ",
+				"  [[[[[[ ",
+				"  ZDLTPG5",
+				"  ]]]]]] ",
+				"         ",
+				"[[[[[[[[ ",
+				"DCQJZRBF6",
+				"]]]]]]]] ",
+				"         ",
+				"[[[[[[[[ ",
+				"RTFMJDBS7",
+				"]]]]]]]] ",
+				"         ",
+				" [[[[[[[ ",
+				" MVTBRHL8",
+				" ]]]]]]] ",
+				"         ",
+				"   [[[[[ ",
+				"   VSDPQ9",
+				"   ]]]]] ",
+			}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			have := TransposeLines(tt.lines)
-			assert.Equal(t, tt.want, have)
+			input := tt.lines
+			expected := tt.want
+			actual := TransposeLines(input)
+			assert.Equal(t, expected, actual)
 		})
 	}
 }
