b4ceff9421ace2df4674cc719f437b5226f38f78
Phil Hanna <ph1204@gmail.com>
2023-11-09 15:20:03 -0500

    Added unit test for file size and name

--- day7/input_handler_test.go
+++ day7/input_handler_test.go
@@ -50,3 +50,31 @@ func TestInputContext_HandleCDParent(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "/a", context.cwd.FullPath())
 }
+
+func TestInputContext_HandleFileSizeAndName(t *testing.T) {
+	tests := []struct {
+		name string
+		cmds []string
+		line string
+	}{
+		{"subfile i",
+			[]string{
+				"$ cd /",
+				"$ cd a",
+				"$ cd e",
+				"584 i",
+			},
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var err error
+			context := NewContext()
+			for _, cmd := range tt.cmds {
+				err = context.HandleLine(cmd)
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
