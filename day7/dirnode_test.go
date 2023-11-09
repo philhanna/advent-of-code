4cda237615c8ecb46c3f656b8425f0ddf32a447e
Phil Hanna <ph1204@gmail.com>
2023-11-09 16:33:33 -0500

    Consolidated GetTestData

--- day7/dirnode_test.go
+++ day7/dirnode_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDirNode_LookupChild(t *testing.T) {
+func GetTestData() (*DirNode, *DirNode, *DirNode, *DirNode) {
 	root := NewDirNode(nil, "/")
 	a := NewDirNode(root, "a")
 	e := NewDirNode(a, "e")
@@ -21,6 +21,12 @@ func TestDirNode_LookupChild(t *testing.T) {
 	NewFileNode(d, "d.log", 8033020)
 	NewFileNode(d, "d.ext", 5626152)
 	NewFileNode(d, "k", 7214296)
+	return root, a, e, d
+}
+
+func TestDirNode_LookupChild(t *testing.T) {
+
+	root, a, e, _ := GetTestData()
 
 	tests := []struct {
 		name      string
@@ -50,20 +56,7 @@ func TestDirNode_LookupChild(t *testing.T) {
 
 func TestDirNode_ChildrenAsStrings(t *testing.T) {
 
-	root := NewDirNode(nil, "/")
-	a := NewDirNode(root, "a")
-	e := NewDirNode(a, "e")
-	NewFileNode(e, "i", 584)
-	NewFileNode(a, "f", 29116)
-	NewFileNode(a, "g", 2557)
-	NewFileNode(a, "h.lst", 62596)
-	NewFileNode(root, "b.txt", 14848514)
-	NewFileNode(root, "c.dat", 8504156)
-	d := NewDirNode(root, "d")
-	NewFileNode(d, "j", 4060174)
-	NewFileNode(d, "d.log", 8033020)
-	NewFileNode(d, "d.ext", 5626152)
-	NewFileNode(d, "k", 7214296)
+	root, a, e, _ := GetTestData()
 
 	tests := []struct {
 		name     string
@@ -99,20 +92,7 @@ func TestDirNode_ChildrenAsStrings(t *testing.T) {
 
 func TestDirNode_Size(t *testing.T) {
 
-	root := NewDirNode(nil, "/")
-	a := NewDirNode(root, "a")
-	e := NewDirNode(a, "e")
-	NewFileNode(e, "i", 584)
-	NewFileNode(a, "f", 29116)
-	NewFileNode(a, "g", 2557)
-	NewFileNode(a, "h.lst", 62596)
-	NewFileNode(root, "b.txt", 14848514)
-	NewFileNode(root, "c.dat", 8504156)
-	d := NewDirNode(root, "d")
-	NewFileNode(d, "j", 4060174)
-	NewFileNode(d, "d.log", 8033020)
-	NewFileNode(d, "d.ext", 5626152)
-	NewFileNode(d, "k", 7214296)
+	_, _, e, d := GetTestData()
 
 	tests := []struct {
 		name     string
