3f83482f87deb8213ee23429d66299439ef6c9aa
Phil Hanna <ph1204@gmail.com>
2023-11-08 19:12:32 -0500

    Added mainline stub

--- day7/cmd/main.go
+++ day7/cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github/philhanna/advent/day7"
+	"fmt"
+	"github/philhanna/advent-of-code/day7"
 )
 
 func main() {
 	root := day7.NewDirNode("/")
+	fmt.Printf("DEBUG: root=%s\n", root)
 }
