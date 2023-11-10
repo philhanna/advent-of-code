6faa3321f5c98edee40e60ad73d8f2d8afde1074
Phil Hanna <ph1204@gmail.com>
2023-11-10 13:16:20 -0500

    Added mainline skeleton

--- day7/main.go
+++ day7/main.go
@@ -2,21 +2,15 @@ package main
 
 import "fmt"
 
-// ---------------------------------------------------------------------
-// Mainline
-// ---------------------------------------------------------------------
+const (
+	FILENAME = "sampleinput.txt"
+	LIMIT = 100000
+)
 
 func main() {
-	root := NewDirNode(nil, "/")
-	a := NewDirNode(root, "a")
-	e := NewDirNode(a, "e")
-
-	file := NewFileNode(e, "bfile.txt", 123)
-	rootFile := NewFileNode(root, "rootFile.txt", 86)
-
-	fmt.Printf("root: %s\n", root)
-	fmt.Printf("rootFile: %s\n", rootFile)
-	fmt.Printf("a   : %s\n", a)
-	fmt.Printf("e   : %s\n", e)
-	fmt.Printf("file: %s\n", file)
+	context := NewContext()
+	err := context.HandleInput(FILENAME)
+	if err != nil {
+		fmt.Printf("Could not read %s: %s\n", FILENAME, err)
+	}
 }
