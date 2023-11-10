770b4deb08058f1ac88f1c0059082230c733b302
Phil Hanna <ph1204@gmail.com>
2023-11-10 13:29:44 -0500

    Got correct answer to part 1

--- day7/main.go
+++ day7/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 const (
-	FILENAME = "sampleinput.txt"
+	FILENAME = "input.txt"
 	LIMIT    = 100000
+)
+
+var (
+	totalSize = 0
 )
 
 func main() {
@@ -13,4 +17,27 @@ func main() {
 	if err != nil {
 		fmt.Printf("Could not read %s: %s\n", FILENAME, err)
 	}
+
+	HandleDirectory(context.root)
+	fmt.Printf("Total size is %d\n", totalSize)
+}
+
+func HandleDirectory(dir *DirNode) {
+	if dir.Size() <= LIMIT {
+		Accept(dir)
+	}
+	for _, child := range dir.children {
+		switch v := child.(type) {
+		case *DirNode:
+			HandleDirectory(v)
+		case *FileNode:
+		default:
+			fmt.Printf("Node %s, unknown type %T\n", child, v)
+		}
+	}	
+}
+
+func Accept(dir *DirNode) {
+	fmt.Printf("Accepted directory %s of size %d\n", dir.FullPath(), dir.Size())
+	totalSize += dir.Size()
 }
