a8f1a28ab2319adb25fe344403275d6d645eb60b
Phil Hanna <ph1204@gmail.com>
2023-11-10 15:57:14 -0500

    Added test function for Tree

--- day7/main.go
+++ day7/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
-	FILENAME = "input.txt"
+	FILENAME = "sampleinput.txt"
 	LIMIT    = 100000
 )
 
@@ -12,6 +15,32 @@ var (
 )
 
 func main() {
+	context := NewContext()
+	err := context.HandleInput(FILENAME)
+	if err != nil {
+		fmt.Printf("Could not read %s: %s\n", FILENAME, err)
+	}
+	Tree(context.root, 0)
+}
+
+func Tree(dir *DirNode, level int) {
+	indent := func(level int) string {
+		return strings.Repeat(" ", 4*level)
+	}
+	fmt.Printf("%s%s\n", indent(level), dir.Name())
+	for _, child := range dir.children {
+		switch v := child.(type) {
+		case *DirNode:
+			Tree(v, level + 1)
+		case *FileNode:
+			fmt.Printf("%s%s\n", indent(level+1), v.Name())
+		default:
+			fmt.Printf("BUG: Unknown file type %s for %s\n", v, child)
+		}
+	}
+}
+
+func Main2() {
 	context := NewContext()
 	err := context.HandleInput(FILENAME)
 	if err != nil {
