9890c9ed4175c5b24ee4786429c1e659539f0e44
Phil Hanna <ph1204@gmail.com>
2023-11-12 21:23:48 -0500

    Tested mainline with sample data

--- day8/cmd1/main.go
+++ day8/cmd1/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github/philhanna/advent-of-code/day8"
+)
+
+const filename = "testdata/sample.dat"
+func main() {
+	data := day8.LoadData(filename)
+	count := data.CountVisible()
+	fmt.Printf("Number of visible trees = %d\n", count)
 }
