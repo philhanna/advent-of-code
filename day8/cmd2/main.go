214c488164786416a2908ab532e211f82c7b6bc1
Phil Hanna <ph1204@gmail.com>
2023-11-12 23:28:16 -0500

    Correct answer to day8 part 2

--- day8/cmd2/main.go
+++ day8/cmd2/main.go
@@ -9,6 +9,18 @@ import (
 const filename = "testdata/input.dat"
 func main() {
 	data := day8.LoadData(filename)
-	count := data.CountVisible()
-	fmt.Printf("Number of visible trees = %d\n", count)
+	most := 0
+	mostRow := 0
+	mostCol := 0
+	for row := 0; row < len(data); row++ {
+		for col := 0; col < len(data); col++ {
+			ss := data.ScenicScore(row, col)
+			if ss > most {
+				most = ss
+				mostRow = row
+				mostCol = col
+			}
+		}
+	}
+	fmt.Printf("Most scenic spot is (%d,%d). Score=%d\n", mostRow, mostCol, most)
 }
