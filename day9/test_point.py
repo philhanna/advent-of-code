fe0953b7fcec9657bfa41f9df5eff9ccdb4a7708
Phil Hanna <ph1204@gmail.com>
2023-11-16 13:45:55 -0500

    Added move and unit tests

--- day9/test_point.py
+++ day9/test_point.py
@@ -5,7 +5,7 @@ from point import Point
 @pytest.mark.parametrize("row1,col1,row2,col2,expected", [
     (1, 5, 2, 6, True),
 ])
-def test_knot_touches(row1, col1, row2, col2, expected):
+def test_touches(row1, col1, row2, col2, expected):
     head = Point(row1, col1)
     tail = Point(row2, col2)
     actual = head.touches(tail)
