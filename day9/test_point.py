bcdf5901dea9308fe1c0d3c3d9200ef12d4a04e9
Phil Hanna <ph1204@gmail.com>
2023-11-16 13:22:55 -0500

    Moved some files

---
+++ day9/test_point.py
@@ -0,0 +1,12 @@
+import pytest
+
+from point import Point
+
+@pytest.mark.parametrize("row1,col1,row2,col2,expected", [
+    (1, 5, 2, 6, True),
+])
+def test_knot_touches(row1, col1, row2, col2, expected):
+    head = Point(row1, col1)
+    tail = Point(row2, col2)
+    actual = head.touches(tail)
+    assert expected == actual
