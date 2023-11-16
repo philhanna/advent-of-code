d550ae70cc0e9752bb18f877d909e9ba63cb951f
Phil Hanna <ph1204@gmail.com>
2023-11-16 13:48:06 -0500

    Added unit test for move

---
+++ day9/test_knot.py
@@ -0,0 +1,21 @@
+import pytest
+
+from point import Point
+from knot import Knot
+
+@pytest.mark.parametrize("row,col,direction,want_row,want_col", [
+    (1, 5, "U", 0, 5),
+    (1, 5, "D", 2, 5),
+    (1, 5, "L", 1, 4),
+    (1, 5, "R", 1, 6),
+])
+def test_move(row, col, direction, want_row, want_col):
+    point = Point(row, col)
+    head = Knot("H", point)
+    head.move(direction)
+    assert want_row == point.row
+    assert want_col == point.col
+
+if __name__ == '__main__':
+    pytest.main()
+
