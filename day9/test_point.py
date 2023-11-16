f785c3544130393110dac7338883317651451f68
Phil Hanna <ph1204@gmail.com>
2023-11-16 17:03:23 -0500

    Added unit tests for touches() method

--- day9/test_point.py
+++ day9/test_point.py
@@ -3,7 +3,13 @@ import pytest
 from point import Point
 
 @pytest.mark.parametrize("row1,col1,row2,col2,expected", [
-    (1, 5, 2, 6, True),
+    (3, 4, 3, 4, True),     # Same point
+    (2, 3, 2, 4, True),     # One to the right
+    (2, 3, 2, 2, True),     # One to the left
+    (2, 3, 1, 3, True),     # Up one row
+    (2, 3, 3, 3, True),     # Down one row
+    (1, 5, 2, 6, True),     # Diagonally adjacent
+    (3, 5, 1, 4, False),    # Too far apart
 ])
 def test_touches(row1, col1, row2, col2, expected):
     head = Point(row1, col1)
