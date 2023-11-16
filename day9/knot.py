be46c4c59f4fcda1d2c673b1a2fed3e92aff1498
Phil Hanna <ph1204@gmail.com>
2023-11-16 13:47:55 -0500

    Added move(self, direction)

--- day9/knot.py
+++ day9/knot.py
@@ -2,4 +2,16 @@ class Knot:
     def __init__(self, name, point):
         self.name = name
         self.point = point
-    
+
+    def move(self, direction):
+        """ Moves the knot in the specified direction """
+        if direction == "U":
+            self.point.row -= 1
+        elif direction == "D":
+            self.point.row += 1
+        elif direction == "L":
+            self.point.col -= 1
+        elif direction == "R":
+            self.point.col += 1
+        else:
+            raise RuntimeError(f"Undefined direction \"{direction}\"")
